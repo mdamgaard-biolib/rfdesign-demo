d7eae5a3d89b8d39be6b2d3452aa7c3fe4f7f26f
Mads D. Pedersen <mdamgaard@biolib.com>
2022-11-30 15:48:19 +0100

    test

--- docker_test.py
+++ docker_test.py
@@ -1,3 +1,5 @@
-import docker
-client = docker.from_env()
-print(client.info(timeout=4000))
+import biolib
+my_env = os.environ.copy()
+if my_env.get('DOCKER_CERT_PATH'):
+    my_env.pop('REQUESTS_CA_BUNDLE', None)
+docker.from_env(timeout=4000, environment=my_env)
