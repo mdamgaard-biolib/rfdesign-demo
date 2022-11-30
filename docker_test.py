efc47073dd8976f9430dde2a8c016ce8a6cb8c98
Mads D. Pedersen <mdamgaard@biolib.com>
2022-11-30 16:01:19 +0100

    fix

--- docker_test.py
+++ docker_test.py
@@ -2,6 +2,6 @@ import biolib
 import os
 import docker
 req = os.environ.pop('REQUESTS_CA_BUNDLE', None)
-docker.from_env(timeout=4000)
+client = docker.from_env(timeout=4000)
 os.environ['REQUESTS_CA_BUNDLE'] = req
-docker.info()
+client.info()
